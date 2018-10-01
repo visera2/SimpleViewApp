0338ce4853613a3a7c4c39c6cbc1cc174b8f6bf0
s.perevoznikov <sperev@bk.ru>
2018-10-01 19:46:52 +0200

    first

---
+++ SingleViewApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingleViewApp
+//
+//  Created by Sergei Perevoznikov on 10/05/2018.
+//  Copyright © 2018 Sergei Perevoznikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
